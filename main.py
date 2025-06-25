24e3693a2ceb31b7992593e99abe95609813ebdd
psps16 <ppbhattuf57@gmail.com>
2025-06-25 15:19:52 +0530

    add env example

--- main.py
+++ main.py
@@ -14,13 +14,11 @@ from fastapi.staticfiles import StaticFiles
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel, Field, EmailStr
-
+from dotenv import load_dotenv
 # --- 1. Security & Configuration ---
 
 # Secret key for encoding/decoding JWTs. In a real app, load this from an environment variable.
-SECRET_KEY = "a_very_secret_key_that_should_be_changed"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+load_dotenv()
 
 # Password hashing context
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
